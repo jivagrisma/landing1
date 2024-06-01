c52096c73416939f85c0681ba09ae581c23c0cbf
Jorge Grisales <111651530+jivagrisma@users.noreply.github.com>
2024-06-01 11:40:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('miFormulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Aquí puedes agregar lo que sucederá cuando se envíe el formulario.
+    alert('Formulario enviado!');
+  });
+  
